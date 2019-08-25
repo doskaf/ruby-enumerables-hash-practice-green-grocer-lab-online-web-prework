99f6418f70e42e3028ec24025d36485f50ba53e7
Dominique Skaf <doskaf@yahoo.com>
2019-08-25 23:54:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,6 +41,6 @@ end
 
 def checkout(array, coupons)
   hash_cart = consolidate_cart(array)
-  applied_coupons = applied_coupons(hash_cart, coupons)
+  applied_coupons = apply_coupons(hash_cart, coupons)
   applied_discount = apply_clearance(applied_coupons)
 end

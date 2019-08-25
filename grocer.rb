76f970745e26f4d0a55cd49528b098ec38add74a
Dominique Skaf <doskaf@yahoo.com>
2019-08-25 23:53:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -42,4 +42,5 @@ end
 def checkout(array, coupons)
   hash_cart = consolidate_cart(array)
   applied_coupons = applied_coupons(hash_cart, coupons)
+  applied_discount = apply_clearance(applied_coupons)
 end

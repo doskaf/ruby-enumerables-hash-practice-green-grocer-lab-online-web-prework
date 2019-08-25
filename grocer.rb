c683e7258f239e919863441116af229947bfbb1e
Dominique Skaf <doskaf@yahoo.com>
2019-08-25 22:47:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,14 @@ def consolidate_cart(cart)
   final_hash = {}
   cart.each do |element_hash|
     element_name = element_hash.keys[0]
+    element_stats = element_hash.values[0]
+    
+    if final_hash.hash_key?(element_name)
+      element_stats[:count] += 1
+      
+    else
+      
+    end
   end
 end
 
